5dead48f24842c72751038ae2ad0c0cab41c9b5f
Umesh Siddarth U S <umeshsiddarth@gmail.com>
2023-03-02 17:42:05 +0530

    Added basic game mechanics like mathematical operation, secret number and checking the answer.

--- Guess the Game/app.js
+++ Guess the Game/app.js
@@ -1,20 +1,41 @@
 const difficultyEl = document.querySelector(".level");
-const p1guessEl = document.querySelector(".player1__guess--number");
 const p1AddEl = document.querySelector(".player1__add");
 const p1SubEl = document.querySelector(".player1__subtract");
 const p1MulEl = document.querySelector(".player1__multiply");
 const p1DivEl = document.querySelector(".player1__divide");
 const p1RollEl = document.querySelector(".player1__roll");
-const p1FeedbackEl = document.querySelector(".player1__feedback");
 const p1AttemptsEl = document.querySelector(".player1__attempts--count");
 const p1DiceOutEl = document.querySelector(".player1__dice-output");
-const secretNumberEl = document.querySelector(".player1__secret-number");
-
+let p1SecretNumberEl = document.querySelector(".player1__secret-number");
+let p1guessEl = document.querySelector(".player1__guess--number");
+let p1FeedbackEl = document.querySelector(".player1__feedback");
 const diceRoll = function () {
   return Math.floor(Math.random() * 6) + 1;
 };
 
+const secretNumberGenerator = function (min, max) {
+  return Math.floor(Math.random() * max) + min;
+};
+
 let guess = 0;
+let secretNumber = secretNumberGenerator(1, 20);
+
+function feedback() {
+  if (
+    p1guessEl.textContent === "0" &&
+    p1FeedbackEl.textContent === "Start guessing..."
+  ) {
+    // Do nothing
+  } else if (secretNumber < guess) {
+    p1FeedbackEl.textContent = "High Number";
+  } else if (secretNumber > guess) {
+    p1FeedbackEl.textContent = "Low Number";
+  } else if (secretNumber === guess) {
+    p1FeedbackEl.textContent = "Correct Guess";
+    p1SecretNumberEl.textContent = secretNumber;
+    document.body.style.backgroundColor = "green";
+  }
+}
 
 p1RollEl.addEventListener("click", () => {
   const diceOut = diceRoll();
@@ -45,9 +66,47 @@ if (p1RollEl.disabled === false) {
 
 p1AddEl.addEventListener("click", () => {
   guess += Number(p1DiceOutEl.textContent);
+  p1guessEl.textContent = guess;
+  feedback();
   p1AddEl.disabled = true;
   p1SubEl.disabled = true;
   p1MulEl.disabled = true;
   p1DivEl.disabled = true;
   p1RollEl.disabled = false;
 });
+
+p1SubEl.addEventListener("click", () => {
+  guess -= Number(p1DiceOutEl.textContent);
+  p1guessEl.textContent = guess;
+  feedback();
+  p1AddEl.disabled = true;
+  p1SubEl.disabled = true;
+  p1MulEl.disabled = true;
+  p1DivEl.disabled = true;
+  p1RollEl.disabled = false;
+});
+
+p1MulEl.addEventListener("click", () => {
+  guess *= Number(p1DiceOutEl.textContent);
+  p1guessEl.textContent = guess;
+  feedback();
+  p1AddEl.disabled = true;
+  p1SubEl.disabled = true;
+  p1MulEl.disabled = true;
+  p1DivEl.disabled = true;
+  p1RollEl.disabled = false;
+});
+
+p1DivEl.addEventListener("click", () => {
+  const temp = guess / Number(p1DiceOutEl.textContent);
+  guess = Math.round(temp);
+  p1guessEl.textContent = guess;
+  feedback();
+  p1AddEl.disabled = true;
+  p1SubEl.disabled = true;
+  p1MulEl.disabled = true;
+  p1DivEl.disabled = true;
+  p1RollEl.disabled = false;
+});
+
+console.log(Math.round(1.567));
